81d4e2a90514fefdbc83721ac11c2e98e56a6bc6
fredy-tan <fredy_tantri@yahoo.co.id>
2025-03-18 15:25:32 +0800

    updated

--- app/src/main/java/com/example/week8/MainActivity.java
+++ app/src/main/java/com/example/week8/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.week8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView tv = findViewById(R.id.SecondText);
         tv.setText("Wow");
+
+        Button btn = findViewById(R.id.btnChangeText);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("INFOSYS!!!");
+            }
+        });
     }
 }
